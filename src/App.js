ac2eef8668c6a906a1abd555769ed672e5ac3713
maudnals <maud.nalpas@gmail.com>
2018-05-27 13:01:26 +0200

    created functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi</h1>
-    //   </div>
-    // );
-    return React.createElement("div", { className: "App" },
-      React.createElement("p", null,
-        React.createElement("button", null, "Click me I'm lazy")
-      )
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+        <Person></Person>
+      </div>
     );
+    // return React.createElement("div", { className: "App" },
+    //   React.createElement("p", null,
+    //     React.createElement("button", null, "Click me I'm lazy")
+    //   )
+    // );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+// class Person extends Component {
+//   render() {
+//     return (
+//       <div>I'm a person for real</div>
+//     );
+//   }
+// }
+
+
+const person = () => {
+  return <p>I'm a person</p>;
+}
+
+export default person; 
