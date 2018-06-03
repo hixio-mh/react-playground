5fe75189f2d56a08077f9280a491f6a211d40657
maudnals <maud.nalpas@gmail.com>
2018-06-03 17:02:42 +0200

    mutate state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Scar",
+        age: 30,
+        extra: null
+      },
+      {
+        name: "Jo",
+        age: 23,
+        extra: "My hobbies: football"
+      },
+      {
+        name: "Maud",
+        age: 27,
+        extra: null
+      }
+    ]
+  }
+
+  increaseAllAgesHandler = () => {
+    console.log("ih");
+    this.setState(Object.assign(
+      this.state,
+      {
+        persons: this.state.persons.map(p => (
+          {
+            ...p,
+            age: p.age + 1
+          }
+        ))
+      }
+    ));
+  }
   render() {
     return (
       <div className="App">
-        <h1>Hi</h1>
-        <Person name="Scar" age="9"></Person>
-        <Person name="Jo" age="10"><h1>My hobbies: football</h1></Person>
-        <Person name="Maud" age="3"></Person>
+        <button onClick={this.increaseAllAgesHandler}></button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+        </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          {this.state.persons[1].extra}
+        </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+        </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,12 @@
-import React, { Component } from 'react';
-
-// class Person extends Component {
-//   render() {
-//     return (
-//       <div>I'm a person for real</div>
-//     );
-//   }
-// }
+import React from 'react';
 
 const person = (props) => {
   return (
     <div>
-      <p>I'm {props.name} and I am {props.age} years old. {6 + 3}</p> 
-      <p>{props.children}</p>
+      <h1>{props.name} ({props.age})</h1>
+      <div>{props.children}</div>
     </div>
-  );
-}
+  )
+};
 
 export default person;
