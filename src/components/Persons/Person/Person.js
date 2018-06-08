7a6b79dc36fa3fec865d32a6e1584d8a067a88aa
maudnals <maud.nalpas@gmail.com>
2018-06-08 15:03:11 +0200

    transformed into stateful just for illustration

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,19 @@
-import React from 'react'; 
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
+class Person extends Component {
 
-  // if (Math.random() > 0.5) {
-  //   throw new Error("Something went wrong");
-  // }
-
-  return (
-    <div className={classes.bluecolored}>
-      <h1>{props.name} ({props.age})</h1>
-      <div>{props.children}</div> 
-      <button onClick={props.increaseAgeHandler}>Increase age</button>
-      <button onClick={props.deletePersonHandler}>Delete me</button>
-      <input type="text" name="" onChange={props.changeNameHandler} defaultValue={props.name}/>
-    </div>
-  )
+  render() {
+    return (
+      <div className={classes.bluecolored}>
+        <h1>{this.props.name} ({this.props.age})</h1>
+        <div>{this.props.children}</div>
+        <button onClick={this.props.increaseAgeHandler}>Increase age</button>
+        <button onClick={this.props.deletePersonHandler}>Delete me</button>
+        <input type="text" name="" onChange={this.props.changeNameHandler} defaultValue={this.props.name} />
+      </div>
+    );
+  }
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map(
-  (p, index) => {
-    return (
-      <Person
-        key={p.id}
-        name={p.name}
-        age={p.age}
-        increaseAgeHandler={props.increaseAgeHandler.bind(this, p)}
-        changeNameHandler={props.changeNameHandler.bind(this, p.id)}
-        deletePersonHandler={props.deletePersonHandler.bind(this, index)}
-      />);
-  });
+class Persons extends Component {
+  render() {
+    return this.props.persons.map(
+      (p, index) => {
+        return (
+          <Person
+            key={p.id}
+            name={p.name}
+            age={p.age}
+            increaseAgeHandler={this.props.increaseAgeHandler.bind(this, p)}
+            changeNameHandler={this.props.changeNameHandler.bind(this, p.id)}
+            deletePersonHandler={this.props.deletePersonHandler.bind(this, index)}
+          />);
+      });
+  }
+}
 
-export default persons;
+export default Persons;
